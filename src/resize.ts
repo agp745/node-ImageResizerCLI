f8d0035150dd34217c14d6b4fe2c81791478f55b
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-08-28 16:11:07 -0500

    ffmpeg script added

--- src/resize.ts
+++ src/resize.ts
@@ -1,6 +1,26 @@
 import fs from 'fs'
+import { exec } from 'child_process'
 
 // ffmpeg -i imageName.jpg -vf scale=20:-1 imageName-small.jpg
+
+function ffmpeg(fileName: string, scale: number = 20): void {
+
+    const splitIdx = fileName.indexOf('.')
+    const outFile = fileName.slice(0, splitIdx)
+
+    exec(`ffmpeg -i ${fileName} -vf scale=${scale}:-1 small-${outFile}.jpg`, 
+    (error, stdout, stderr) => {
+        if(error) {
+            console.error(`Error: ${error.message}`)
+            return
+        }
+        if(stderr) {
+            console.error(`Error: ${stderr}`)
+            return
+        }
+        console.log(`images resized successfully\noutput: ${stdout}`)
+    })
+}
 
 export function resize(folderPath: string) {
     try {
@@ -8,7 +28,7 @@ export function resize(folderPath: string) {
 
         filesArr.forEach((file) => {
             try {
-                console.log(file)
+                ffmpeg(file)
             } catch(err) {
                 if(err instanceof Error) {
                     throw new Error (err.message)
