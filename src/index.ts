dbe829d210d74d854b1eac6b52c35671fca47622
Alex Perez <alexgp@unknown020fb5692481.attlocal.net>
2023-08-28 13:27:02 -0500

    directory reader added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello')
+#!/usr/bin/env node
+
+import fs from 'fs'
+import { Command } from 'commander'
+
+const program = new Command()
+
+const dir_files = fs.readdir(__dirname, (err, files) => {
+    if(err) throw new Error (err.message)
+    console.log(files)
+    return files
+})
+
+console.log(__dirname ,dir_files)
