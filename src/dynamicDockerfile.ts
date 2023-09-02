71f6c940110337288c1658e8fbb26d74fa23b61e
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-09-02 15:05:35 -0500

    version 1.0 +  post docker processes added

--- src/dynamicDockerfile.ts
+++ src/dynamicDockerfile.ts
@@ -2,6 +2,19 @@ import fs from 'fs'
 import { spawn } from 'child_process'
 import color from 'console-log-colors'
 import { getErrorMessage } from './utils/errorMessage'
+import { inherits } from 'util'
+
+const buildCmd = `docker build -t resizecli-image -f ./Dockerfile.dev .`
+const buildArgs = buildCmd.split(' ').splice(1)
+
+const runCmd = `docker run --rm -v .:/app resizecli-image`
+const runArgs = runCmd.split(' ').splice(1)
+
+const removeImageCmd = 'docker rmi resizecli-image'
+const removeImageArgs = removeImageCmd.split(' ').splice(1)
+
+const removeFilesCmd = "find . -name 'Dockerfile.dev' | xargs rm"
+const removeFilesArgs = ['-c', removeFilesCmd]
 
 export function createDockerfile(path: string, scale: number = 20): void {
 
@@ -33,36 +46,48 @@ export function createDockerfile(path: string, scale: number = 20): void {
         }
 
         console.log(color.cyanBright(`✨ 🐳 Dockerfile created!`))
-
-        buildImage(path)
+        buildImage()
     })
 }
 
-function buildImage(path: string): void {
-    const buildCmd = `docker build -t resizecli-image -f ./Dockerfile.dev .`
+function buildImage(): void {
     console.log(color.yellow('building image...'), color.dim(buildCmd))
 
-    const buildArgs = buildCmd.split(' ').splice(1)
     const build = spawn('docker', buildArgs, {stdio: 'inherit'})
-
     build.on('close', (code) => {
         if(code === 0) {
             console.log('\n')
-            runImage(path)
+            runImage()
         }
     })
 }
 
-function runImage(path: string) {
-    const runCmd = `docker run --rm -v .:/app resizecli-image`
+function runImage() {
     console.log(color.yellow('running image...'), color.dim(runCmd))
 
-    const runArgs = runCmd.split(' ').splice(1)
     const run = spawn('docker', runArgs, {stdio: 'inherit'})
 
     run.on('close', (code, signal) => {
         if(code === 0) {
-            spawn('docker', ['rmi', 'resizecli-image'], {stdio: 'inherit'})
+            removeDocker()
+        } else {
+            console.log(color.red(`process exited with code: ${code}\nsignal: ${signal}`))
+        }
+    })
+}
+
+function removeDocker() {
+    console.log(color.yellow('removing image...'), color.dim(removeImageCmd))
+
+    const rmi = spawn('docker', removeImageArgs, {stdio: 'inherit'})
+    rmi.on('close', (code, signal) => {
+        if(code === 0) {
+            console.log(color.yellow('removing Dockerfile.dev...'), color.dim(removeFilesCmd))
+            const rmFile = spawn('/bin/sh', removeFilesArgs, {stdio: 'inherit'})
+
+            rmFile.on('close', (code) => {
+                if(code === 0) console.log(color.cyanBright('🐳  Dockerfile.dev removed'))
+            })
         } else {
             console.log(color.red(`process exited with code: ${code}\nsignal: ${signal}`))
         }
